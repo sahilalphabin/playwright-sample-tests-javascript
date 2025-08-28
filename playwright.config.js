99be74411cb62680742c1ca210ade3c1d1bcdde3
Alphabin <alphabin@Alphabins-Mac-mini.local>
2025-08-28 16:35:09 +0530

    Initial commit: Playwright tests with TestDino integration

---
+++ playwright.config.js
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+const isCI = !!process.env.CI;
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: isCI,
+  retries: isCI ? 1 : 0,
+  workers: isCI ? 1 : 1,
+
+  timeout: 60 * 1000, // ⏱️ each test fails after 1 min
+  // In CI we only show a list reporter. The workflow sets --reporter=blob.
+  // Locally you also get HTML and JSON.
+  reporter: [
+    ['html', {
+      outputFolder: 'playwright-report',
+      open: 'never'
+    }],
+    ['blob', { outputDir: 'blob-report' }], // Use blob reporter
+    ['json', { outputFile: './playwright-report/report.json' }],
+  ],
+
+  use: {
+    baseURL: 'https://demo.alphabin.co/',
+    headless: false,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
